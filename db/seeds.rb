a1bbd4c409387a9cc994e918f93a800a375a5681
ng <nogpdx@gmail.com>
2017-12-18 14:22:39 -0800

    seed displaying tour title, description, date, price

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,11 @@
+Tour.destroy_all
+
 20.times do |index|
   Tour.create!(title: Faker::Zelda.game,
                   price: rand(10...100),
-                  description: Faker::Lorem.sentence(5, false,0).chop
+                  description: Faker::Lorem.sentence(5, false,0).chop,
+                  location: Faker::Zelda.location,
+                  date: Faker::Date.between(2.days.ago, Date.today))
   end
+
+p "Created #{Tour.count} tours"
