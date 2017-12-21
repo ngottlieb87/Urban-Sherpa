87ba9afbbc57fc4c260f0440c5f5e2cc3bdd5bae
ng <nogpdx@gmail.com>
2017-12-21 08:37:31 -0800

    add show for guide avatar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 User.destroy_all
 Tour.destroy_all
 
-20.times do |index|
+10.times do |index|
   User.create!( email: Faker::Internet.email ,
                 user_name:Faker::Hacker.verb,
                 admin: false,
@@ -11,14 +11,14 @@ Tour.destroy_all
 
   end
 
-20.times do |index|
+10.times do |index|
   Tour.create!( title: Faker::Zelda.game,
                 photo: URI.parse(Faker::LoremPixel.image),
                 price: rand(10...100),
                 description: Faker::Lorem.sentence(5, false,0).chop,
                 location: Faker::Zelda.location,
                 date: Faker::Date.between(2.days.ago, Date.today),
-                user_id: rand(1...20))
+                user_id: rand(1...10))
   end
 
 p "Created #{User.count} users"
