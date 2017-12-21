bedf65871a37f920de2d3f24a70b3b5e0ca1723c
ng <nogpdx@gmail.com>
2017-12-20 16:34:07 -0800

    removed public system for ignore file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ User.destroy_all
 Tour.destroy_all
 
 20.times do |index|
-  User.create!( email: Faker::Internet.email,
-                user_name: Faker::Internet.user_name,
+  User.create!( email: Faker::Hacker.verb + "@gmail.com" ,
+                user_name:Faker::Hacker.verb,
                 admin: false,
                 guide: true,
                 password: "Password1",
