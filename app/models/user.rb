e670a66320ce8e8c2b0340b0ca99fffa10c4fa49
ngottlieb87 <nogpdx@gmail.com>
2018-02-19 14:29:56 -0800

    fix sign in for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,26 +5,16 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates_confirmation_of :password, presence: true
   validate :password_complexity
-  validate :email_match
   attr_accessor :password
   before_save :encrypt_password
   has_attached_file :avatar, styles: { small: "80x90>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
-
 
   def password_complexity
     if password.present?
        if !password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{7,15}$/)
          errors.add :password, "Password complexity requirement not met"
        end
-    end
-  end
-
-  def email_match
-    if email.present?
-      if !email.match(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/)
-        errors.add :email, "Email not valid!"
-      end
     end
   end
 
