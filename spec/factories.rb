44caa2d45087857f7ed1776145165e0dd98a7ca2
ngottlieb87 <nogpdx@gmail.com>
2018-05-04 13:53:27 -0700

    passing tour auth specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -8,6 +8,14 @@ FactoryBot.define do
     avatar{ fixture_file_upload(Rails.root.join('spec', 'images', 'ghost.png'), 'image/png') }
   end
 
+  factory(:attendee, class: User) do
+    user_name('lil')
+    email('h@h.com')
+    password('Password2')
+    guide(false)
+    avatar{ fixture_file_upload(Rails.root.join('spec', 'images', 'ghost.png'), 'image/png') }
+  end
+
   factory(:account) do
     user_id 1
   end
---
+++ spec/features/tour_auth_fail_spec.rb
@@ -0,0 +1,42 @@
+require 'rails_helper'
+
+describe 'fail authorizations for tour' do
+  it 'fail auth routing for tour creation', js: true do
+    visit 'tours#index'
+    click_on 'Sign In'
+    user = FactoryBot.create(:attendee)
+    fill_in 'email', :with => user.email
+    fill_in 'password', :with => user.password
+    click_button "Sign in"
+    visit new_tour_path
+    expect(page).to have_content("Must Be A Guide To Create Tours")
+  end
+
+  it 'fail tour authorization edit',js:true do
+    visit 'tours#index'
+    click_on 'Sign In'
+    user = FactoryBot.create(:user)
+    fill_in 'email', :with => user.email
+    fill_in 'password', :with => user.password
+    click_button "Sign in"
+    visit new_tour_path
+    fill_in 'tour_title', :with => "Night Hike"
+    fill_in "tour_description", :with => "A hike at night"
+    fill_in "tour_location", :with => "Portland,Or"
+    fill_in "tour_date", :with=> "2019/01/01"
+    find("#tour_time_4i").find(:option, "12 AM").select_option
+    find("#tour_time_5i").find(:option, "00").select_option
+    fill_in "tour_price", :with => 50
+    attach_file 'tour_photo', "#{Rails.root}/spec/images/party.jpeg"
+    click_on "Create Tour"
+    visit '/signout'
+    visit 'tours#index'
+    click_on 'Sign In'
+    user = FactoryBot.create(:attendee)
+    fill_in 'email', :with => user.email
+    fill_in 'password', :with => user.password
+    click_button "Sign in"
+    visit "tours/#{Tour.first.id}/edit"
+    expect(page).to have_content "Not your tour to update"
+  end
+end
