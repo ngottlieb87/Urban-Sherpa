d4a67dba8d0c994cb87a8ac943187d922bd6dcba
ngottlieb87 <nogpdx@gmail.com>
2018-05-14 11:40:29 -0700

    passing comment edit spec

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -22,6 +22,7 @@ describe "user comment spec" do
     click_button "Leave Comment"
     expect(page).to have_content "Comment Added!"
   end
+
   it "comment creation fail", js:true do
     visit 'tours#index'
     click_on 'Sign In'
@@ -42,5 +43,21 @@ describe "user comment spec" do
     fill_in "comment_body", :with => " "
     click_button "Leave Comment"
     expect(page).to have_content "Comment field can't be blank"
+  end
+
+  it "lets user edit comment", js: true do
+    visit 'tours#index'
+    click_on 'Sign In'
+    user = FactoryBot.create(:user)
+    FactoryBot.create(:tour)
+    FactoryBot.create(:comment)
+    fill_in 'email', :with => user.email
+    fill_in 'password', :with => user.password
+    click_button "Sign in"
+    visit "tours/#{Tour.first.id}"
+    click_on "Edit Comment"
+    fill_in 'comment_body', with: "New Comment"
+    click_button "Leave Comment"
+    expect(page).to have_content "New Comment"
   end
 end
