65a8b9e275689947ae2b8d65b74815dea440860f
ngottlieb87 <nogpdx@gmail.com>
2018-06-13 16:16:49 -0700

    Revert "Mail gun"

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,14 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "mail@urbansherpa.com"
-
-  def sign_up_email(user)
-    @user=user
-    mail(to: @user.email, subject: 'DO NOT REPLY: Thank for creating an account')
-  end
-
-  def reciept_email(email,tour_order,amount)
-    @tour_orders=tour_order
-    @amount= amount
-    mail(to: email, subject: 'Thank You For Your Purchase!')
-  end
+  default from: 'from@example.com'
+  layout 'mailer'
 end
